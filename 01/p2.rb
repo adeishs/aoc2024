7d8f2cf2b745f813a420a93daac6490f724b211c
ade ishs <ade@adeishs.com>
2024-12-01 16:30:39 +1100

    Use fewer var

--- 01/p2.rb
+++ 01/p2.rb
@@ -5,7 +5,11 @@ lines = $stdin.each_line
               .map { |line| line.chomp.split }.flatten
 
 lefts = lines.each_slice(2).map { |p| p.first.to_i }
-rights = lines.each_slice(2).map { |p| p.last.to_i }
-freq = Hash[*rights.group_by { |v| v }.flat_map { |k, v| [k, v.size] }]
+freq = Hash[
+  *lines.each_slice(2)
+        .map { |p| p.last.to_i }
+        .group_by { |v| v }
+        .flat_map { |k, v| [k, v.size] }
+]
 
 puts lefts.map { |l| l * (freq[l] || 0) }.sum
