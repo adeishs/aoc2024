799c3eb60811e08bb8d772126369c80c9473d7b9
ade ishs <ade@adeishs.com>
2024-12-01 17:47:22 +1100

    Shorten code

--- 01/p1.rb
+++ 01/p1.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-lines = $stdin.each_line
-              .map { |line| line.chomp.split }.flatten
-
-lefts = lines.each_slice(2).map { |p| p.first.to_i }.sort
-rights = lines.each_slice(2).map { |p| p.last.to_i }.sort
+lefts, rights = $stdin.each_line
+                      .map { |line| line.chomp.split.map(&:to_i) }
+                      .flatten
+                      .partition.with_index { |_, i| i.even? }
+                      .map(&:sort)
 
 puts lefts.map.with_index { |l, i| (l - rights[i]).abs }.sum
--- 01/p2.rb
+++ 01/p2.rb
@@ -1,15 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-lines = $stdin.each_line
-              .map { |line| line.chomp.split }.flatten
+lefts, rights = $stdin.each_line
+                      .map { |line| line.chomp.split.map(&:to_i) }
+                      .flatten
+                      .partition.with_index { |_, i| i.even? }
 
-lefts = lines.each_slice(2).map { |p| p.first.to_i }
-freq = Hash[
-  *lines.each_slice(2)
-        .map { |p| p.last.to_i }
-        .group_by { |v| v }
-        .flat_map { |k, v| [k, v.size] }
-]
+freq = Hash[*rights.group_by { |v| v }.flat_map { |k, v| [k, v.size] }]
 
 puts lefts.map { |l| l * (freq[l] || 0) }.sum
