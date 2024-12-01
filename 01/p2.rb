e2c96719f1a445048f188fda8d1c649d55528ee6
ade ishs <ade@adeishs.com>
2024-12-01 19:35:58 +1100

    Reduce time complexity

--- 01/p2.rb
+++ 01/p2.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-lefts, rights = $stdin.each_line
-                      .map { |line| line.chomp.split.map(&:to_i) }
-                      .flatten
-                      .partition.with_index { |_, i| i.even? }
+lefts = []
+freq = Hash.new(0)
+$stdin.each_line
+      .map { |line| line.chomp.split.map(&:to_i) }
+      .each do |l, r|
+        lefts << l
+        freq[r] += 1
+      end
 
-freq = Hash[*rights.group_by { |v| v }.flat_map { |k, v| [k, v.size] }]
-
-puts lefts.map { |l| l * (freq[l] || 0) }.sum
+puts lefts.map { |l| l * freq[l] }.sum
