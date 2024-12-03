9a02afdf248bad0aae6071f539c50cf58d80b697
ade ishs <adei@logicly.com.au>
2024-12-03 18:56:45 +1100

    Modularise

--- 03/p2.rb
+++ 03/p2.rb
@@ -6,18 +6,20 @@ F = {
   "don't()" => 0
 }.freeze
 
-def parse(line)
-  f = 1
-  line.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/)
-      .map do |ts|
-        t = ts.shift
-        if t.start_with?('m')
-          f * t.scan(/\d+/).map(&:to_i).reduce(:*)
-        else
-          f = F[t]
-          0
-        end
-      end
+def get_tokens(line)
+  line.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/).map(&:shift)
 end
 
-puts parse($stdin.each_line.map(&:chomp).to_a.join('')).sum
+def parse(line)
+  f = 1
+  get_tokens(line).map do |t|
+    if t.start_with?('m')
+      f * t.scan(/\d+/).map(&:to_i).reduce(:*)
+    else
+      f = F[t]
+      0
+    end
+  end
+end
+
+puts parse($stdin.each_line.map(&:chomp).join).sum
