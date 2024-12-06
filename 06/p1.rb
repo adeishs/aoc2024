5ee090de8dae9c839f8387ce7fd6f5c140bacd5e
ade ishs <adei@logicly.com.au>
2024-12-07 06:50:36 +1100

    Modularise

--- 06/p1.rb
+++ 06/p1.rb
@@ -13,21 +13,27 @@ def find_start(rows)
       .first
 end
 
+def move(rows, curr_pos, dir)
+  new_pos = curr_pos + dir
+  return nil unless new_pos.imag.between?(0, rows.size - 1) &&
+                    new_pos.real.between?(0, rows.first.size - 1)
+
+  if OBST[rows[new_pos.imag][new_pos.real]]
+    dir *= 0 + 1i
+  else
+    curr_pos += dir
+  end
+  [curr_pos, dir]
+end
+
 def run_guard(rows)
   curr_pos = find_start(rows)
   visiteds = Set.new
   dir = 0 + -1i
   loop do
     visiteds << curr_pos
-    new_pos = curr_pos + dir
-    break unless new_pos.imag.between?(0, rows.size - 1) &&
-                 new_pos.real.between?(0, rows.first.size - 1)
-
-    if OBST[rows[new_pos.imag][new_pos.real]]
-      dir *= 0 + 1i
-    else
-      curr_pos += dir
-    end
+    curr_pos, dir = move(rows, curr_pos, dir)
+    break unless curr_pos
   end
 
   visiteds.size
