70f1a1465c61225359887dd32a14f42d5d6623c7
ade ishs <ade@adeishs.com>
2024-12-08 05:46:55 +1100

    Add day 7 part 1

---
+++ 07/p1.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+OPS = %w[+ *].freeze
+
+def parse(line)
+  test, operand_str = line.split(': ')
+  operands = operand_str.split.map(&:to_i)
+
+  { test: test.to_i, operands: operands }
+end
+
+def calculate(operators, equation)
+  result = 0
+  (['+'] + operators).each.with_index do |o, i|
+    result = result.send(o, equation[:operands][i])
+  end
+
+  result
+end
+
+def tested?(equation)
+  OPS.repeated_permutation(equation[:operands].size - 1)
+     .each do |ops|
+       return true if equation[:test] == calculate(ops, equation)
+     end
+
+  false
+end
+
+puts $stdin.each_line.map { |l| parse(l.chomp) }
+           .select { |e| tested?(e) }
+           .map { |e| e[:test] }
+           .sum
