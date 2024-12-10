2540980e6a9cc9f16d89ffcc4b65c051dcfdc094
ade ishs <adei@logicly.com.au>
2024-12-11 09:17:25 +1100

    Modularise

--- 10/p1.rb
+++ 10/p1.rb
@@ -5,38 +5,45 @@ require 'set'
 
 DIRS = [1, -1].map { |d| [d + 0i, d * 1i] }.flatten.freeze
 
-locs = Hash.new { |h, k| h[k] = Set.new }
-heights = $stdin.each_line
-                .map
-                .with_index do |line, y|
-                  line.chomp
-                      .each_char
-                      .map
-                      .with_index do |hs, x|
-                        h = hs.to_i
-                        locs[h] << Complex(x, y)
-                        h
-                      end
-                end
-
-reachable_peaks = Hash.new { |h, k| h[k] = Set.new }
-
-9.downto(0).each do |h|
-  locs[h].each do |loc|
-    reachable_peaks[loc] = Set[loc] if h == 9
-
-    DIRS.map { |d| loc + d }
-        .select do |next_loc|
-          nx = next_loc.real
-          ny = next_loc.imag
-          ny.between?(0, heights.size - 1) &&
-            nx.real.between?(0, heights.first.size - 1) &&
-            heights[ny][nx] == h - 1
-        end
-        .each do |next_loc|
-          reachable_peaks[next_loc] |= reachable_peaks[loc]
-        end
-  end
+def reachable_loc?(heights, curr_height, next_loc)
+  nx = next_loc.real
+  ny = next_loc.imag
+  ny.between?(0, heights.size - 1) &&
+    nx.between?(0, heights.first.size - 1) &&
+    heights[ny][nx] == curr_height - 1
 end
 
-puts locs[0].map { |l| reachable_peaks[l].size }.sum
+def count_reachable_peaks(heights, locs)
+  reachable_peaks = Hash.new { |h, k| h[k] = Set.new }
+
+  9.downto(0).each do |h|
+    locs[h].each do |loc|
+      reachable_peaks[loc] = Set[loc] if h == 9
+
+      DIRS.map { |d| loc + d }
+          .select { |next_loc| reachable_loc?(heights, h, next_loc) }
+          .each { |next_loc| reachable_peaks[next_loc] |= reachable_peaks[loc] }
+    end
+  end
+
+  locs[0].map { |l| reachable_peaks[l].size }.sum
+end
+
+def parse(lines)
+  locs = Hash.new { |h, k| h[k] = Set.new }
+  heights = lines.map
+                 .with_index do |line, y|
+                   line.chomp
+                       .each_char
+                       .map
+                       .with_index do |hs, x|
+                         h = hs.to_i
+                         locs[h] << Complex(x, y)
+                         h
+                       end
+                 end
+
+  [heights, locs]
+end
+
+puts count_reachable_peaks(*parse($stdin.each_line))
--- 10/p2.rb
+++ 10/p2.rb
@@ -5,38 +5,45 @@ require 'set'
 
 DIRS = [1, -1].map { |d| [d + 0i, d * 1i] }.flatten.freeze
 
-locs = Hash.new { |h, k| h[k] = Set.new }
-heights = $stdin.each_line
-                .map
-                .with_index do |line, y|
-                  line.chomp
-                      .each_char
-                      .map
-                      .with_index do |hs, x|
-                        h = hs.to_i
-                        locs[h] << Complex(x, y)
-                        h
-                      end
-                end
-
-trail_counts = Hash.new(0)
-
-9.downto(0).each do |h|
-  locs[h].each do |loc|
-    trail_counts[loc] = 1 if h == 9
-
-    DIRS.map { |d| loc + d }
-        .select do |next_loc|
-          nx = next_loc.real
-          ny = next_loc.imag
-          ny.between?(0, heights.size - 1) &&
-            nx.real.between?(0, heights.first.size - 1) &&
-            heights[ny][nx] == h - 1
-        end
-        .each do |next_loc|
-          trail_counts[next_loc] += trail_counts[loc]
-        end
-  end
+def reachable_loc?(heights, curr_height, next_loc)
+  nx = next_loc.real
+  ny = next_loc.imag
+  ny.between?(0, heights.size - 1) &&
+    nx.between?(0, heights.first.size - 1) &&
+    heights[ny][nx] == curr_height - 1
 end
 
-puts locs[0].map { |l| trail_counts[l] }.sum
+def count_trails(heights, locs)
+  trail_counts = Hash.new(0)
+
+  9.downto(0).each do |h|
+    locs[h].each do |loc|
+      trail_counts[loc] = 1 if h == 9
+
+      DIRS.map { |d| loc + d }
+          .select { |next_loc| reachable_loc?(heights, h, next_loc) }
+          .each { |next_loc| trail_counts[next_loc] += trail_counts[loc] }
+    end
+  end
+
+  locs[0].map { |l| trail_counts[l] }.sum
+end
+
+def parse(lines)
+  locs = Hash.new { |h, k| h[k] = Set.new }
+  heights = lines.map
+                 .with_index do |line, y|
+                   line.chomp
+                       .each_char
+                       .map
+                       .with_index do |hs, x|
+                         h = hs.to_i
+                         locs[h] << Complex(x, y)
+                         h
+                       end
+                 end
+
+  [heights, locs]
+end
+
+puts count_trails(*parse($stdin.each_line))
