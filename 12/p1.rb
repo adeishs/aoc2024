5e35abf2e75c0f1d343dbe7f53d06fabc4cb8cb0
ade ishs <ade@adeishs.com>
2024-12-12 23:31:04 +1100

    Make it more FP-ish

--- 12/p1.rb
+++ 12/p1.rb
@@ -5,15 +5,19 @@ require 'set'
 
 DIRS = [1, -1].map { |n| [n + 0i, n * 1i] }.flatten.freeze
 
+def in_map?(loc, max_loc)
+  %w[
+    imag real
+  ].all? { |m| loc.send(m).between?(0, max_loc.send(m)) }
+end
+
 def find_whole_region(plant, loc, plants, plant_locs)
   max_loc = Complex(plants.first.size - 1, plants.size - 1)
 
   DIRS.map { |d| loc + d }
       .each do |adj|
         next unless !plant_locs.member?(adj) &&
-                    %w[
-                      imag real
-                    ].all? { |m| adj.send(m).between?(0, max_loc.send(m)) } &&
+                    in_map?(adj, max_loc) &&
                     plants[adj.imag][adj.real] == plant
 
         plant_locs << adj
@@ -42,18 +46,12 @@ region_locs = {}
 end
 
 fences = region_locs.values.uniq.map do |plant_locs|
-  fence_cnt = 0
-  plant_locs.each do |loc|
-    DIRS.map { |d| loc + d }
-        .each do |adj|
-          next if plant_locs.member?(adj) &&
-                  %w[
-                    imag real
-                  ].none? { |m| !adj.send(m).between?(0, max_loc.send(m)) }
-
-          fence_cnt += 1
-        end
-  end
+  fence_cnt =
+    plant_locs.map do |loc|
+      DIRS.map { |d| loc + d }
+          .reject { |adj| in_map?(adj, max_loc) && plant_locs.member?(adj) }
+          .size
+    end.sum
 
   { region_size: plant_locs.size, fence_cnt: fence_cnt }
 end
