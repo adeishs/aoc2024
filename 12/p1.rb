d2321e4a47007ce62e413f2e3b4ee944dc55f451
ade ishs <ade@adeishs.com>
2024-12-12 23:01:38 +1100

    Add day 12 part 1

--- 12/p1.rb
+++ 12/p1.rb
@@ -3,33 +3,59 @@
 
 require 'set'
 
-def assign_region(regions, loc, region)
-  return 1 unless regions[loc].nil?
+DIRS = [1, -1].map { |n| [n + 0i, n * 1i] }.flatten.freeze
 
-  regions[loc] = region
-  [
-    1, -1, 0 + 1i, 0 + -1i
-  ].map { |d| loc + d }
-    .select { |l| l.imag.between?(0, regions.size - 1) && l.real.between?(0, regions.first.size - 1) }
-    .select { |loc| loc }
-end
+def find_whole_region(plant, loc, plants, plant_locs)
+  max_loc = Complex(plants.first.size - 1, plants.size - 1)
 
-def fence(plants)
-  fences = Hash.new { |h, k| h[k] = Set.new }
-  regions = {}
-  plants.each.with_index do |row, y|
-    row.chars.each.with_index do |p, x|
-      fences[Complex(x, y)] |= (
-        (y.zero? || p != plants[y - 1][x] ? [0 + -1i] : []) +
-        (y == plants.size - 1 || p != plants[y + 1][x] ? [0 + 1i] : []) +
-        (x.zero? || p != plants[y][x - 1] ? [-1 + 0i] : []) +
-        (x == plants[y].size - 1 || p != plants[y][x + 1] ? [1 + 0i] : [])
-      ).flatten
-    end
-  end
+  DIRS.map { |d| loc + d }
+      .each do |adj|
+        next unless !plant_locs.member?(adj) &&
+                    %w[
+                      imag real
+                    ].all? { |m| adj.send(m).between?(0, max_loc.send(m)) } &&
+                    plants[adj.imag][adj.real] == plant
 
-  fences
+        plant_locs << adj
+        plant_locs.merge(find_whole_region(plant, adj, plants, plant_locs))
+      end
+
+  plant_locs
 end
 
 plants = $stdin.each_line.map(&:chomp)
-puts fence(plants)
+max_loc = Complex(plants.first.size - 1, plants.size - 1)
+visited_locs = Set.new
+region_locs = {}
+
+(0..max_loc.imag).each do |y|
+  (0..max_loc.real).each do |x|
+    start = Complex(x, y)
+    next if visited_locs.member?(start)
+
+    plant = plants[y][x]
+    plant_locs = Set[start]
+    plant_locs = find_whole_region(plant, start, plants, plant_locs)
+    region_locs[start] = plant_locs
+    visited_locs << plant_locs
+  end
+end
+
+fences = region_locs.values.uniq.map do |plant_locs|
+  fence_cnt = 0
+  plant_locs.each do |loc|
+    DIRS.map { |d| loc + d }
+        .each do |adj|
+          next if plant_locs.member?(adj) &&
+                  %w[
+                    imag real
+                  ].none? { |m| !adj.send(m).between?(0, max_loc.send(m)) }
+
+          fence_cnt += 1
+        end
+  end
+
+  { region_size: plant_locs.size, fence_cnt: fence_cnt }
+end
+
+puts fences.map { |f| f.values.reduce(1, :*) }.sum
