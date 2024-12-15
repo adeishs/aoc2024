5b11b075c02387bfd951e094e5f21e373a948cbb
ade ishs <ade@adeishs.com>
2024-12-16 08:01:35 +1100

    WIP. Add warehouse expansion

---
+++ 15/p2.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+ROBOT = '@'
+BOX = 'O'
+BOX_L = '['
+BOX_R = ']'
+WALL = '#'
+EMPTY = '.'
+DIR = {
+  '^' => 0 + -1i,
+  'v' => 0 + 1i,
+  '>' => 1 + 0i,
+  '<' => -1 + 0i
+}.freeze
+
+def update_tile(rows, loc, tile)
+  rx, ry = loc.rect
+  rr = rows[ry]
+
+  rows[0...ry] +
+    ["#{rr[0...rx]}#{tile}#{rr[rx + 1...rr.size]}"] +
+    rows[ry + 1...rows.size]
+end
+
+def expand_tile(tile)
+  tile == BOX ? [BOX_L, BOX_R] : ([tile] * 2)
+end
+
+def expand_warehouse(warehouse)
+  {
+    init_loc: warehouse[:init_loc] + warehouse[:init_loc].real,
+    mvts: warehouse[:mvts],
+    rows: warehouse[:rows].map do |row|
+      row.chars.each_with_object([]) { |t, ts| ts.concat(expand_tile(t)) }.join
+    end
+  }
+end
+
+def parse_input(inp)
+  map_str, mvt_str = inp.split("\n\n")
+
+  rows = map_str.split("\n")
+  init_loc = Complex(
+    *rows.map.with_index { |row, y| { x: row.index(ROBOT), y: y } }
+         .reject { |c| c[:x].nil? }
+         .map { |c| [c[:x], c[:y]] }
+         .first
+  )
+
+  expand_warehouse(
+    {
+      init_loc: init_loc,
+      rows: update_tile(rows, init_loc, EMPTY),
+      mvts: mvt_str.chars.each_with_object([]) do |m, ds|
+              d = DIR[m]
+              ds << d unless d.nil?
+            end
+    }
+  )
+end
+
+def obj_at?(rows, loc, obj)
+  rows[loc.imag][loc.real] == obj
+end
+
+def move_box(rows, loc, dir)
+  nl = loc + dir
+  # no-op if hitting a wall
+  return [loc, rows] if obj_at?(rows, nl, WALL)
+
+  empty_found = false
+  empty_loc = nl
+  until empty_found
+    break if obj_at?(rows, empty_loc, WALL)
+
+    if obj_at?(rows, empty_loc, EMPTY)
+      empty_found = true
+    else
+      empty_loc += dir
+    end
+  end
+
+  return [loc, rows] unless empty_found
+
+  [nl, update_tile(update_tile(rows, empty_loc, BOX), nl, EMPTY)]
+end
+
+def move_robot(loc, dir, rows)
+  nl = loc + dir
+  # no-op if hitting a wall
+  return [loc, rows] if obj_at?(rows, nl, WALL)
+
+  # move and nothing else if empty
+  return [nl, rows] if obj_at?(rows, nl, EMPTY)
+
+  move_box(rows, loc, dir)
+end
+
+def run_warehouse(warehouse)
+  loc = warehouse[:init_loc]
+  rows = warehouse[:rows]
+
+  warehouse[:mvts].each { |d| loc, rows = move_robot(loc, d, rows) }
+  rows
+end
+
+def calc_row_gps_sum(row, idx)
+  row.chars.map.with_index { |t, x| t == BOX_L ? 100 * idx + x : 0 }.sum
+end
+
+#puts #run_warehouse(
+puts  parse_input($stdin.read)
+#)# .map.with_index { |row, y| calc_row_gps_sum(row, y) }.sum
