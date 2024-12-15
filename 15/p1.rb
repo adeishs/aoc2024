8dfe5a6722ef226b5682cd49148e8bdaef1501f1
ade ishs <ade@adeishs.com>
2024-12-16 07:12:40 +1100

    Shorten code by exploiting walls

--- 15/p1.rb
+++ 15/p1.rb
@@ -24,7 +24,7 @@ end
 def parse_input(inp)
   map_str, mvt_str = inp.split("\n\n")
 
-  rows = map_str.split("\n")[1...-1].map { |r| r[1...-1] }
+  rows = map_str.split("\n")
   init_loc = Complex(
     *rows.map.with_index { |row, y| [row.index(ROBOT), y] }
          .reject { |cs| cs.first.nil? }
@@ -41,23 +41,19 @@ def parse_input(inp)
   }
 end
 
-def in_map?(loc, max_loc)
-  loc.rect.zip(max_loc.rect).all? { |l, m| l.between?(0, m) }
-end
-
 def obj_at?(rows, loc, obj)
   rows[loc.imag][loc.real] == obj
 end
 
-def move_box(rows, loc, dir, max_loc)
+def move_box(rows, loc, dir)
   nl = loc + dir
   # no-op if hitting a wall
-  return [loc, rows] if !in_map?(nl, max_loc) || obj_at?(rows, nl, WALL)
+  return [loc, rows] if obj_at?(rows, nl, WALL)
 
   empty_found = false
   empty_loc = nl
   until empty_found
-    break if !in_map?(empty_loc, max_loc) || obj_at?(rows, empty_loc, WALL)
+    break if obj_at?(rows, empty_loc, WALL)
 
     if obj_at?(rows, empty_loc, EMPTY)
       empty_found = true
@@ -71,29 +67,27 @@ def move_box(rows, loc, dir, max_loc)
   [nl, update_tile(update_tile(rows, empty_loc, BOX), nl, EMPTY)]
 end
 
-def move_robot(loc, dir, rows, max_loc)
+def move_robot(loc, dir, rows)
   nl = loc + dir
   # no-op if hitting a wall
-  return [loc, rows] if !in_map?(nl, max_loc) || obj_at?(rows, nl, WALL)
+  return [loc, rows] if obj_at?(rows, nl, WALL)
 
   # move and nothing else if empty
   return [nl, rows] if obj_at?(rows, nl, EMPTY)
 
-  move_box(rows, loc, dir, max_loc)
+  move_box(rows, loc, dir)
 end
 
 def run_warehouse(warehouse)
   loc = warehouse[:init_loc]
   rows = warehouse[:rows]
-  max_loc = Complex(rows.first.size - 1, rows.size - 1)
 
-  warehouse[:mvts].each { |d| loc, rows = move_robot(loc, d, rows, max_loc) }
+  warehouse[:mvts].each { |d| loc, rows = move_robot(loc, d, rows) }
   rows
 end
 
 def calc_row_gps_sum(row, idx)
-  row.chars
-     .map.with_index { |t, x| t == BOX ? (100 * (idx + 1) + x + 1) : 0 }.sum
+  row.chars.map.with_index { |t, x| t == BOX ? 100 * idx + x : 0 }.sum
 end
 
 puts run_warehouse(
