7c9d885299d9a3df4ec1e736b68da6f6ae33f4be
ade ishs <ade@adeishs.com>
2024-12-22 17:39:12 +1100

    DRY

--- 22/p1.rb
+++ 22/p1.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+def mix_rinse(num, shl)
+  ((num << shl) ^ num) & 0xffffff
+end
+
 def get_next_secret_num(num)
-  num = ((num << 6) ^ num) & 0xffffff
-  num = ((num >> 5) ^ num) & 0xffffff
-  ((num << 11) ^ num) & 0xffffff
+  mix_rinse(mix_rinse(mix_rinse(num, 6), -5), 11)
 end
 
 def get_final_secret_num(num)
